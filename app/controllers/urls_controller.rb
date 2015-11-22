2aa275b7d31debca1adfb67bec28bba2f69567bb
zirexba <zirexba@hotmail.com>
2015-11-22 10:42:58 -0500

    Functional 1

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,12 +1,16 @@
 class UrlsController < ApplicationController
+	before_action :click
 
 	def index
 		@url=Url.new
 		@urls=Url.all.order("id DESC").take(10)
+
 	end
 	def create
 		@url=Url.new(url_params)
 		@url.short = SecureRandom.urlsafe_base64(5)
+		@url.clicks = 0
+		@url.active =1
 		if @url.save
 			redirect_to root_path
 		end
@@ -15,5 +19,15 @@ class UrlsController < ApplicationController
 	private
 	def url_params
 		params.require(:url).permit(:url_text,:short,:clicks,:active)
+	end
+	def click
+	    @p= request.original_url.split('/')[3]
+	    if @p.present? 
+			@ur=Url.find_by_short(@p)
+			if @ur.present?
+				@ur.update_attributes(clicks: @ur.clicks+1)
+				redirect_to root_path
+			end
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'urls#index'
   resources :urls
+
+  get '/:id' => 'urls#index', as: :test 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
