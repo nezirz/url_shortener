e0fb0b2d9117074fcd957a7508e2f1bbe9aa3dc0
zirexba <zirexba@hotmail.com>
2016-01-20 07:05:00 -0500

    beta 1

--- Gemfile
+++ Gemfile
@@ -31,7 +31,8 @@ gem 'bootstrap-sass', '~> 3.3.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+ gem "better_errors"
+ gem 'pry-byebug'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -44,4 +45,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,15 +4,32 @@ class UrlsController < ApplicationController
 	def index
 		@url=Url.new
 		@urls=Url.all.order("id DESC").take(10)
-
+		@urlvisits=Url.all.order("clicks DESC").take(10)
+		#@short=request.original_url+session[:short]
+		@short = request.original_url.gsub('?','')
 	end
 	def create
 		@url=Url.new(url_params)
-		@url.short = SecureRandom.urlsafe_base64(5)
-		@url.clicks = 0
-		@url.active =1
-		if @url.save
-			redirect_to root_path
+		@p = Url.find_by_url_text(@url.url_text)
+
+		 binding.pry
+		
+		if @p.present?
+			#session[:short] = @p.short
+			@u=root_path+'?'+@p.short
+			redirect_to @u
+		else
+			@url.short = SecureRandom.urlsafe_base64(5)
+			@url.clicks = 0
+			@url.active =1
+
+			if @url.save
+
+				#session[:short] = @url.short
+				@u=root_path+"?"+@url.short
+				redirect_to @u
+				return
+			end
 		end
 	end
 
@@ -20,14 +37,16 @@ class UrlsController < ApplicationController
 	def url_params
 		params.require(:url).permit(:url_text,:short,:clicks,:active)
 	end
+
 	def click
 	    @p= request.original_url.split('/')[3]
-	    if @p.present? 
+	    #binding.pry
+
 			@ur=Url.find_by_short(@p)
 			if @ur.present?
 				@ur.update_attributes(clicks: @ur.clicks+1)
 				redirect_to root_path
 			end
-		end
+
 	end
 end
